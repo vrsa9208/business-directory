0fbc0d81b4f53196afd7ecd04480d5d74c3ef699
vrsa9208 <vrsa9208@gmail.com>
2021-08-16 13:34:50 -0500

    Basic redux setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Suspense } from "react";
+import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "./redux/configureStore";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import BusinessList from "./views/BusinessList/BusinessList";
 import BusinessDetails from "./views/BusinessDetails/BusinessDetails";
 import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 import MainLayout from "./components/MainLayout/MainLayout";
 
+const store = configureStore();
+
 const App = () => {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Provider store={store}>
         <CssBaseline />
         <MainLayout>
           <HashRouter>
@@ -25,7 +29,7 @@ const App = () => {
             </Switch>
           </HashRouter>
         </MainLayout>
-      </Suspense>
+      </Provider>
     </>
   );
 };
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import businessReducer from "./businessReducer";
+
+const rootReducer = combineReducers({
+  business: businessReducer,
+});
+
+export default rootReducer;
